93f6cb5fa5a943ec6c4403a423623ba711564d9a
Bikrant Bidari <64511430+bikrantbdr@users.noreply.github.com>
2023-02-02 22:53:59 +0545

    Revert ""any_rooms" bug fixed"

--- Backend/controllers/hostelController.js
+++ Backend/controllers/hostelController.js
@@ -53,7 +53,6 @@ exports.register_hostel = async (req, res, next) => {
     }
 
     const user = await User.findById(decodedToken.id);
-    // const user = await User.findById(body.id);
 
     const hostel = new Hostel({
         name: body.name,
@@ -66,7 +65,6 @@ exports.register_hostel = async (req, res, next) => {
         images: body.images || [],
         verified: body.verified,
         owner: user._id,
-        // owner: body.id,
         amenities: body.amenities || [],
         rooms: body.rooms || [],
         reviews: []
