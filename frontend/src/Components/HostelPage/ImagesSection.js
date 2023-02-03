df805b32e4c199e5e28dd7bde7ed48379a3f03a6
bikrantbdr <bikrantbdr@gmail.com>
2023-02-03 07:53:37 +0545

    added map to individual pages

--- frontend/src/Components/HostelPage/ImagesSection.js
+++ frontend/src/Components/HostelPage/ImagesSection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Map, Marker } from "pigeon-maps";
 import './ImagesSection.css'
 
 function ImagesSection({ hostel }) {
@@ -11,6 +11,12 @@ function ImagesSection({ hostel }) {
         <div className='images__gallery'>
             <img src={ hostel.images[1] } width="200px" alt='Hostel Image' />
             <img src={ hostel.images[2] } width="200px" alt='Hostel Image' />
+        </div>
+        <div className='images__map'>
+          {console.log([hostel.location.coordinates[1],hostel.location.coordinates[0]])}
+        <Map height={300} defaultCenter={[hostel.location.coordinates[1],hostel.location.coordinates[0]]} defaultZoom={14} >
+            <Marker width={50} anchor={[hostel.location.coordinates[1],hostel.location.coordinates[0]]} />
+        </Map>
         </div>
     </div>
   )
