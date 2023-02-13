9f945ff9b4a7c084e8366c773403e145006ef229
bikrantbdr <bikrantbdr@gmail.com>
2023-02-13 16:04:03 +0545

    file management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Routes, Route } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+    </Routes>
   )
 }
 
