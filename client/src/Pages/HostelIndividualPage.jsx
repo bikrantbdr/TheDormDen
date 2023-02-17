e9b223b9b52dc062644505a6247f70da27fc57f1
bikrantbdr <bikrantbdr@gmail.com>
2023-02-17 20:40:57 +0545

    added an individual page

---
+++ client/src/Pages/HostelIndividualPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const HostelIndividualPage = () => {
+  return (
+    <div>HostelIndividualPage</div>
+  )
+}
+
+export default HostelIndividualPage
